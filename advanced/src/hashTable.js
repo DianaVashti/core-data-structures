6199886966a87c5d50168dcf675812ce6b2fb7e5
Diana Vashti <diana.vashti@gmail.com>
2017-04-18 11:51:40 -0700

    Adds more functionality to old linked list with tests. Ports necessary functions to the linkedList that will be used by the hash table. Finalizes Hash function for has table.

--- advanced/src/hashTable.js
+++ advanced/src/hashTable.js
@@ -1,14 +1,148 @@
-const ht = new HashTable()
-
-function Node(data) {
-  this.data = data
+function Node(key, value) {
+  this.key = key
   this.value = value
   this.next = null
+}
+
+export default class LinkedList {
+  constructor() {
+    this.length = 0
+    this.head = null
+  }
+
+  clear() {
+    this.length = 0
+    this.head = null
+  }
+
+  insert(key, value) {
+    let node = new Node(key, value)
+    let currentNode = this.head
+
+    if (!currentNode) {
+      this.head = node
+      this.length++
+      return node
+    }
+    while (currentNode.next) {
+      currentNode = currentNode.next
+    }
+    currentNode.next = node
+    this.length += 1
+    return node
+  }
+
+//regular find
+  find(key) {
+    var currentNode = this.head
+    if (!currentNode) {
+      return null
+    }
+    while (currentNode) {
+      if (currentNode.key === key){
+        return currentNode
+      } else {
+        currentNode = currentNode.next
+      }
+    } return -1
+  }
+
+//finds the node whose next value is the target node
+  findByNext(key) {
+    let currentNode = this.head
+
+    if(!currentNode) {
+      return null
+    } else if (!currentNode.next && currentNode.key === key){
+      return clear()
+    } else {
+      while(currentNode.next){
+        if(currentNode.next.key === key){
+          return currentNode
+        } else {
+          currentNode = currentNode.next
+        }
+      } return null
+    }
+  }
+
+//cleaner remove using findByNext
+  removeWithFindByNext(key) {
+    let currentNode = this.head
+
+    const findByNext = function(key) {
+      if(!currentNode) {
+        return null
+      } else if (!currentNode.next && currentNode.key === key){
+        return clear()
+      } else {
+        while(currentNode.next){
+          if(currentNode.next.key === key){
+            return currentNode
+          } else {
+            currentNode = currentNode.next
+          }
+        } return null
+      }
+    }
+
+    let foundNode = findByNext(key)
+    if (foundNode === null){
+      return null
+    } else {
+      foundNode.next = foundNode.next.next
+      return this.length--
+    }
+  }
+//brute force
+  removeIt(key) {
+    let currentNode = this.head
+
+    if(!currentNode){
+      return null
+    } else {
+      while(currentNode){
+        if(currentNode.key === key){
+          if(!currentNode.next){
+            this.head = null
+            return this.length--
+          } else {
+            currentNode = currentNode.next
+            this.length--
+            return this.head = currentNode
+          }
+        } else if (currentNode.next.key === key){
+          currentNode.next = currentNode.next.next
+          return this.length--
+        } else if (currentNode.next !== null){
+          currentNode = currentNode.next
+        } else {
+          return null
+        }
+      }
+    }
+  }
+
 }
 
 export default class HashTable {
   constructor() {
     this.length = 0
+    this.table = {}
+  }
+
+  //ht.hash("name")
+    // generates a hash for the key "name"
+  hash( str ) {
+    const inputString = str
+    const strToArray = inputString.toLowerCase().split('')
+    const alphabit = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    const hashed = []
+
+    for( var i = 0; i < strToArray.length; i++ ){
+      hashed.push(alphabit.indexOf(strToArray[i]) * (i + 1) * 13)
+    }
+    return ( hashed.reduce(( a,b ) => a + b, 0 ) % 101 )
   }
 
   //ht.size()
@@ -19,12 +153,27 @@ export default class HashTable {
 
   //ht.put("name", "Zanzibar")
     // adds a key-value pair to the hash table, deal with collisions using chaining
-  put() {
+  put(key, value) {
+    const node = new Node(key, value)
+    const table = this.table
+    const hashedKey = hash(key)
 
+    if( this.length = 0 ){
+      table.hashedKey = node
+      return this.length = 1
+    } else {
+      if( hash(key) in this.table ){
+        const collisionNode =
+
+      } else {
+        table.hashedKey = node
+        return this.length++
+      }
+    }
   }
 
   //ht.get("name")
-    // returns the data associated with key.
+    // returns the key associated with key.
   get() {
 
   }
@@ -45,20 +194,6 @@ export default class HashTable {
     // removes a key-value pair by key.
   remove(key){
 
-  }
-
-  //ht.hash("name")
-    // generates a hash for the key "name"
-  hash( str ) {
-    const inputString = str
-    const strToArray = inputString.toLowerCase().split('')
-    const alphabit = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    const hashed = []
-
-    for( var i = 0; i < strToArray.length; i++ ){
-      hashed.push(alphabit.indexOf(strToArray[i]) * (i + 1) * 13)
-    }
-    return ( hashed.reduce(( a,b ) => a + b, 0 ) % 101 )
   }
 
 }
