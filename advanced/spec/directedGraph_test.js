06b7437c40d56b9b0b114de80d0b789576e0afbe
Diana Vashti <diana.vashti@gmail.com>
2017-04-20 11:01:34 -0700

    Makes files for directed graph

---
+++ advanced/spec/directedGraph_test.js
@@ -0,0 +1,13 @@
+import chai, { expect } from 'chai'
+import DirectedGraph from '../src/directedGraph'
+
+describe('DirectedGraph', () => {
+
+  it('exists', () => {
+    expect(DirectedGraph).to.be.a('function')
+  })
+
+
+
+
+})
